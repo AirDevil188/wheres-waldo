9225c8c9a73c855e7cdc92a0e55dfe307f2ac7c9
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-02-06 12:41:29 +0100

    add: handleCharMenu function, with positioning

--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -1,11 +1,24 @@
+import { useRef, useState } from "react";
 import styles from "./Game.module.css";
 const Game = () => {
+  const [charMenu, setCharMenu] = useState(false);
+  const ref = useRef(null);
+
+  const handleCharMenu = (e) => {
+    setCharMenu(!charMenu);
+    ref.current.style.display = "block";
+    ref.current.style.left = e.pageX + "px";
+    ref.current.style.right = e.pageX + "px";
+    ref.current.style.top = e.pageY + "px";
+    ref.current.style.bottom = e.pageY + "px";
+  };
+
   return (
     <>
-      <section className={styles.gameSection}>
-        <img src="../src/assets/artworks/artwork_bg.webp" alt="" />
+      <section className={styles.gameSection} onClick={handleCharMenu}>
+        <img src="../src/assets/artworks/artwork_bg.webp" alt="Game Artwork" />
       </section>
-      <section className={styles.characterSelectMenu}>
+      <section className={styles.characterSelectMenu} ref={ref}>
         <form action="" method="post">
           <button>Example</button>
           <button>Example</button>
