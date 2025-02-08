05a56634cc84998512a25ddb14139a4c9af979df
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-02-08 14:13:20 +0100

    add: error checking

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -3,6 +3,10 @@ const db = require("../db/queries");
 
 const getAllGameLevels = asyncHandler(async (req, res, next) => {
   const levels = await db.getAllGameLevels();
+
+  if (levels.length < 1) {
+    return res.status(404).json({ message: "Levels not found" });
+  }
   return res.json(levels);
 });
 
