9c4dc50ceb82ca8273c30791c11f84095742938f
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-02-08 14:24:04 +0100

    add: loader getLevels

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,20 +1,25 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { getLevels } from "../utils/loaders";
 import App from "../App";
 import StartScreen from "../components/StartScreen";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 const Router = () => {
-  const router = createBrowserRouter([
-    {
-      element: <App />,
-      children: [
-        {
-          path: "/",
-          element: <StartScreen />,
-        },
-      ],
-    },
-  ]);
-
+  const router = createBrowserRouter(
+    [
+      {
+        element: <App />,
+        path: "/",
+        children: [
+          {
+            element: <StartScreen />,
+            path: "/",
+            loader: getLevels,
+          },
+        ],
+      },
+    ],
+    {}
+  );
   return (
     <>
       <RouterProvider router={router} />
