05ded47174ad4b3c2b5028e10fcf7f4d35f5a448
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-02-08 14:44:54 +0100

    add: cors middleware and intiazed gameRouter path

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 
+const gameRouter = require("./routes/gameRouter");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: "GET,PUT,POST,DELETE",
+    optionsSuccessStatus: 204,
+  })
+);
+app.use("/", gameRouter);
 
 app.listen(process.env.PORT, () =>
   console.log(`App is listening on the ${process.env.PORT} `)
 );
+
+console.log("where i am here to start new journey....");
