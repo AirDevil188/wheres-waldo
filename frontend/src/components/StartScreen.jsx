4bb12246ae2b9333fc1acc360940ceba51ef0694
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-02-08 15:32:06 +0100

    add: StratScreen component

--- frontend/src/components/StartScreen.jsx
+++ frontend/src/components/StartScreen.jsx
@@ -1,33 +1,27 @@
+import { useLoaderData } from "react-router";
 import styles from "./StartScreen.module.css";
 
 const StartScreen = () => {
+  const levels = useLoaderData();
   return (
     <main>
       <section className={styles.startScreenSection}>
         <section className={styles.headingSection}>
           <h3>Choose a level: </h3>
         </section>
-        <a href="game/1">
-          <article className={styles.boxContainer}>
-            <section className={styles.gameNameSection}>
-              <p>EASY</p>
-            </section>
-          </article>
-        </a>
-        <a href="game/2">
-          <article className={styles.boxContainer}>
-            <section className={styles.gameNameSection}>
-              <p>MEDIUM</p>
-            </section>
-          </article>
-        </a>
-        <a href="game/3">
-          <article className={styles.boxContainer}>
-            <section className={styles.gameNameSection}>
-              <p>HARD</p>
-            </section>
-          </article>
-        </a>
+        {levels
+          ? levels.map((level) => {
+              return (
+                <a href={`game/${level.id}`} key={level.id}>
+                  <article className={styles.boxContainer}>
+                    <section className={styles.gameNameSection}>
+                      <p>{level.level}</p>
+                    </section>
+                  </article>
+                </a>
+              );
+            })
+          : null}
       </section>
     </main>
   );
