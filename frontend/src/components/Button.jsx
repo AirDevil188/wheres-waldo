f894811b2d94a3d7b3949e513c3eaccced6eef66
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-02-08 14:16:16 +0100

    add: props and props validation

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,16 +1,25 @@
 import PropTypes from "prop-types";
 
-const Button = ({ text, type, onClick }) => {
+const Button = ({ text, type, onClick, id, value, className }) => {
   return (
-    <button type={type} onClick={onClick}>
+    <button
+      type={type}
+      onClick={onClick}
+      value={value}
+      id={id}
+      className={className}
+    >
       {text}
     </button>
   );
 };
 
 Button.propTypes = {
-  text: PropTypes.string.isRequired,
+  text: PropTypes.string,
   type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  className: PropTypes.string,
+  id: PropTypes.string,
   onClick: PropTypes.func,
 };
 
