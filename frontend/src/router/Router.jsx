fa3aa720952fd317d2e308bc33f8ab4171826f4a
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-02-19 12:41:30 +0100

    add: routes for Game component with action and loader

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,25 +1,29 @@
-import { getLevels } from "../utils/loaders";
+import { getLevel, getLevels } from "../utils/loaders";
 import App from "../App";
 import StartScreen from "../components/StartScreen";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Game, { handleSubmitTarget } from "../components/Game";
 
 const Router = () => {
-  const router = createBrowserRouter(
-    [
-      {
-        element: <App />,
-        path: "/",
-        children: [
-          {
-            element: <StartScreen />,
-            path: "/",
-            loader: getLevels,
-          },
-        ],
-      },
-    ],
-    {}
-  );
+  const router = createBrowserRouter([
+    {
+      element: <App />,
+      path: "/",
+      children: [
+        {
+          element: <StartScreen />,
+          path: "/",
+          loader: getLevels,
+        },
+        {
+          element: <Game />,
+          path: "/game/:id",
+          loader: getLevel,
+          action: handleSubmitTarget,
+        },
+      ],
+    },
+  ]);
   return (
     <>
       <RouterProvider router={router} />
