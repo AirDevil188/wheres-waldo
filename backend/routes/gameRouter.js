f0688a42e9134d65a8983607a663b290f4c474b4
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-02-21 10:47:21 +0100

    add: route for gameController

--- backend/routes/gameRouter.js
+++ backend/routes/gameRouter.js
@@ -8,4 +8,6 @@ gameRouter.get("/", gameController.getAllGameLevels);
 
 gameRouter.get("/game/:id", gameController.getGameLevel);
 
+gameRouter.post("/game/:id", gameController.validateTarget);
+
 module.exports = gameRouter;
