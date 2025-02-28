f68a9c272bac98ccc4b9a9fa572c6c11a7df3031
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-02-28 18:21:51 +0100

    add: new cords and imageUrls for targets

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -19,17 +19,58 @@ async function startUp() {
         {
           level: "easy",
           imageUrl:
-            "https://res.cloudinary.com/ddxkbe6lh/image/upload/v1738928321/wheres_waldo/levels/mrnprcsbvutjkvspurse.webp",
+            "https://res.cloudinary.com/ddxkbe6lh/image/upload/v1740312900/wheres_waldo/levels/mftze9ln3armpi57s8ia.webp",
         },
         {
           level: "medium",
           imageUrl:
-            "https://res.cloudinary.com/ddxkbe6lh/image/upload/v1738928321/wheres_waldo/levels/qefhrkvx1zrdlnlfneor.webp",
+            "https://res.cloudinary.com/ddxkbe6lh/image/upload/v1740312900/wheres_waldo/levels/mftze9ln3armpi57s8ia.webp",
         },
         {
           level: "hard",
           imageUrl:
-            "https://res.cloudinary.com/ddxkbe6lh/image/upload/v1738928321/wheres_waldo/levels/vd4wndr795ecfgr8khvw.webp",
+            "https://res.cloudinary.com/ddxkbe6lh/image/upload/v1740312900/wheres_waldo/levels/mftze9ln3armpi57s8ia.webp",
+        },
+      ],
+    });
+
+    await prisma.target.createMany({
+      data: [
+        {
+          name: "Geralt",
+          image:
+            "https://res.cloudinary.com/ddxkbe6lh/image/upload/v1740314858/wheres_waldo/characters/nxtar8ujnmtu6jaoviia.webp",
+          xCordMax: 70,
+          xCordMin: 67,
+          yCordMax: 54,
+          yCordMin: 52,
+        },
+        {
+          name: "Worm",
+          image:
+            "https://res.cloudinary.com/ddxkbe6lh/image/upload/v1740314858/wheres_waldo/characters/dyhi5fdaxtgjhiia2n75.webp",
+          xCordMax: 17,
+          xCordMin: 16,
+          yCordMax: 57,
+          yCordMin: 57,
+        },
+        {
+          name: "Metroid Fusion",
+          image:
+            "https://res.cloudinary.com/ddxkbe6lh/image/upload/v1740314858/wheres_waldo/characters/oiau7oat5siljs7xlwbv.webp",
+          xCordMax: 63,
+          xCordMin: 62,
+          yCordMax: 89,
+          yCordMin: 88,
+        },
+        {
+          name: "Link",
+          image:
+            "https://res.cloudinary.com/ddxkbe6lh/image/upload/v1740314858/wheres_waldo/characters/ahw5ljboihvxiwufcui4.webp",
+          xCordMax: 23,
+          xCordMin: 22,
+          yCordMax: 90,
+          yCordMin: 89,
         },
       ],
     });
