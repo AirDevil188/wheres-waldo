28ac985330255d12148d10ba2ff57d9dabb9e42c
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-03-17 13:00:41 +0100

    remove: if statement

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -12,9 +12,6 @@ const validateUser = [
 const getAllGameLevels = asyncHandler(async (req, res, next) => {
   const levels = await db.getAllGameLevels();
 
-  if (levels.length < 1) {
-    return res.status(404).json({ message: "Levels not found" });
-  }
   return res.json(levels);
 });
 
