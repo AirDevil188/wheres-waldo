21548acc479f89187bf6a784557ba13a055789f1
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-03-24 17:20:45 +0100

    add: fetching urls

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://192.168.1.99:5173"],
     methods: "GET,PUT,POST,DELETE",
     optionsSuccessStatus: 204,
   })
--- frontend/src/utils/handleFetch.jsx
+++ frontend/src/utils/handleFetch.jsx
@@ -1,4 +1,6 @@
-const host = "http://localhost:3000";
+const host =
+  import.meta.env.NODE_ENV === "test" ? "http://192.168.1.99:3000" : "railway";
+// ;
 
 export const handleFetch = async (endpoint, input = undefined, method) => {
   const options = {
