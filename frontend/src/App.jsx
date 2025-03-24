9fae5948909d0a0db300a09ef49c0fa8a79693e6
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-03-24 17:16:30 +0100

    add: Footer component in the root app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <Outlet />;
+  return (
+    <>
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
